d84e86e79e32d34f11253d3c2f03367440480bc5
angle136 <angieleevcd@gmail.com>
2026-09-26 00:12:04 +0800

    refactor: simplify armor protocol layers

--- referee_main/apps/referee_main/single_board/armor_link.h
+++ referee_main/apps/referee_main/single_board/armor_link.h
@@ -35,7 +35,7 @@ typedef struct
 } armor_link_diagnostics_t;
 
 void armor_link_init(armor_link_packet_callback_t callback);
-void armor_link_set_debug_mode(uint8_t enabled);
+void armor_link_set_debug_mode_all(uint8_t enabled);
 void armor_link_set_debug_mode_port(uint8_t port_id, uint8_t enabled);
 void armor_link_process(uint8_t port_id, const uint8_t *data, size_t length);
 void armor_link_get_diagnostics(uint8_t port_id,
--- referee_main/apps/referee_main/single_board/robot_control.h
+++ referee_main/apps/referee_main/single_board/robot_control.h
@@ -20,8 +20,5 @@ void referee_control_reset_system(void);
 void referee_control_start_adc_debug(void);
 void referee_control_stop_adc_debug(void);
 uint8_t referee_control_is_adc_debug_active(void);
-void referee_control_start_adc_debug(void);
-void referee_control_stop_adc_debug(void);
-uint8_t referee_control_is_adc_debug_active(void);
 
 #endif /* _REFEREE_MAIN_CONTROL_H_ */
